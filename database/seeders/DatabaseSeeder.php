e6a32ff72cbaf3488f5a9248eb8feb04f66f0a06
NIIT-Anand <Anand.Pilania@niitmts.com>
2025-06-06 09:46:20 +0530

    added base;

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Product::factory(1000)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Order::factory(500)->create()->each(function ($order) {
+            OrderItem::factory(rand(1, 5))->create([
+                'order_id' => $order->id,
+                'product_id' => Product::inRandomOrder()->first()->id,
+            ]);
+        });
     }
 }
